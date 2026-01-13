425a3fef2777eda06c610fc26100a7f22578c483
VikramSingh-vartagram <vikram.singh@vartagram.com>
2026-01-13 14:44:52 +0530

    Fix error in login component

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import useAuthStore from '../store/useAuthstore'
+import useAuthStore from '../store/useAuthStore';
 import Loader from '../components/Loader';
 import { Outlet, Navigate } from 'react-router-dom';
 const ProtectedRoute = () => {
