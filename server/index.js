6efb7655ac29de91af1ba15f60430cea743d6a16
VikramSingh-vartagram <vikram.singh@vartagram.com>
2026-01-13 14:28:39 +0530

    Fix port for render deployment

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const io = new Server(server,{
 
 socketManager(io);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3004;
 
 server.listen(PORT, ()=>{
     console.log(`Synapse running on ${PORT}`);
